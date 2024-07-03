d49bf3719a8148df1436babf574109e47e5b9d8e
Elton J D Gonçalves <elton.goncalves@southsystem.com.br>
2024-07-03 13:28:53 -0300

    ajustes

--- src/main/java/com/cvortex/live/repository/UnidadeDeNegocioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cvortex.live.repository;
-
-import com.cvortex.live.model.UnidadeDeNegocio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UnidadeDeNegocioRepository extends JpaRepository<UnidadeDeNegocio, Integer> {
-}
